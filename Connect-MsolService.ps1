2b015a4be154959e8cecfbac39fee903fd1cc7f0
Luis Gutierrez <luisgutierrez707@gmail.com>
2021-03-09 15:37:16 -0800

    Create Connect-MsolService.ps1

---
+++ Connect-MsolService.ps1
@@ -0,0 +1,39 @@
+Connect-MsolService
+ 
+$Users = Get-MsolUser -EnabledFilter EnabledOnly
+ 
+#Gather domain variables
+$Dom = Get-ADDomain
+ 
+#Establish random password length
+[Reflection.Assembly]::LoadWithPartialName(“System.Web”)
+$RandPassLength = [int] 16
+ 
+#Specify which OU the accounts should be created in
+#Replace 'User Accounts' with the name of the OU that should be used.
+$OU = 'OU=User Accounts,' + $Dom.DistinguishedName
+ 
+$Users | Foreach {
+   
+    #Generate a unique, randomized password for each user
+    $RandomPassword = [System.Web.Security.Membership]::GeneratePassword($RandPassLength, 2)
+      $setpass = ConvertTo-SecureString -AsPlainText $RandomPassword -force
+ 
+    #Generate a new SamAccountName
+    $SamAccountName = $_.FirstName + '.' + $_.LastName
+ 
+    #Generate a new Name
+    $Name = $_.FirstName + ' ' + $_.LastName
+ 
+    #Create new on premises user for existing, enabled cloud user
+    New-ADUser $Name `
+          -AccountPassword $SetPass `
+             -ChangePasswordAtLogon $False `
+            -SamAccountName $SamAccountName `
+        -DisplayName $Name `
+        -GivenName $_.FirstName `
+        -Surname $_.LastName `
+             -UserPrincipalName $_.UserPrincipalName `
+             -Path $OU `
+   
+    }
